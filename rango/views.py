b4e24b19a94ed654f64be6d2cb7ce11c3df3df1b
Callum Cooper Lee <callumcooperlee@gmail.com>
2024-01-19 16:46:16 +0000

    Added about page functionality and links to and fro the index page. (TWD 3.6 Excercise)

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Index Page <br><br><a href='http://127.0.0.1:8000/rango/about/'>Go to about page</a>")
+
+def about(request):
+    return HttpResponse("About Page <br><br><a href='http://127.0.0.1:8000/rango/'>Go to index page</a>")
