f76493e9709a1d932bfaacf15ef35b592bf32afa
Callum Cooper Lee <callumcooperlee@gmail.com>
2024-01-19 17:04:42 +0000

    Created a template for the index page

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Index Page <br><br><a href='http://127.0.0.1:8000/rango/about/'>Go to about page</a>")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("About Page <br><br><a href='http://127.0.0.1:8000/rango/'>Go to index page</a>")
+    return HttpResponse("About Page <br><br><a href='/rango/'>Go to index page</a>")
