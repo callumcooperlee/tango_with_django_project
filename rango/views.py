f9a2149dedc5cb271b7df238d3659fd8d29e53f4
Callum Cooper Lee <2758297C@student.gla.ac.uk>
2024-01-21 15:25:34 +0000

    4.4 - Added template for 'about' page. Template includes examples of both media and static files

--- rango/views.py
+++ rango/views.py
@@ -6,4 +6,5 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("About Page <br><br><a href='/rango/'>Go to index page</a>")
+    context_dict = {'boldmessage': 'This tutorial was completed by Callum Cooper Lee'}
+    return render(request, 'rango/about.html', context=context_dict)
